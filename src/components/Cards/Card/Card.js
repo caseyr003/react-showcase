1bdae1c6c0eb03c1e7b8a5aa1946784348483345
Casey Ross <caseyross003@gmail.com>
2018-12-25 20:37:18 -0600

    update card component ui

--- src/components/Cards/Card/Card.js
+++ src/components/Cards/Card/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Radium from 'radium';
 import styles from './Card.module.css';
 
 const card = (props) => {
@@ -11,15 +12,23 @@ const card = (props) => {
   }
 
   const backgroundStyle ={
-    backgroundImage: `url(${background})`
-  }
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.20), rgba(0, 0, 0, 0.50)),
+      url(${background})`,
+    transition: `0.5s`,
+    ':hover': {
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.50)),
+      url(${background})`
+    }
+  };
 
   return (
     <div style={backgroundStyle} className={styles.Card}>
-      <h1>{props.title}</h1>
-      <h4>{props.subtitle}</h4>
+      <div className={styles.Content}>
+        <h1>{props.title}</h1>
+        <h4>{props.subtitle}</h4>
+      </div>
     </div>
   )
 };
 
-export default card;
+export default Radium(card);
